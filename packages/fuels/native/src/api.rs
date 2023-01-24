a9af502ccefd41bb2bc3c515231bed26cb7df69c
mpoplavkov <mihail.poplavkov@gmail.com>
2023-01-24 18:36:12 +0100

    Proper wallet unlocked creation methods

--- packages/fuels/native/src/api.rs
+++ packages/fuels/native/src/api.rs
@@ -8,8 +8,8 @@ pub use fuels::prelude::WalletUnlocked as NativeWalletUnlocked;
 use fuels_signers::wallet::DEFAULT_DERIVATION_PATH_PREFIX;
 use tokio::runtime::Runtime;
 
-use crate::model::transaction;
 pub use crate::model::provider::*;
+use crate::model::transaction;
 
 pub struct Balances {
     pub assets: Vec<String>,
@@ -19,25 +19,44 @@ pub struct Balances {
 pub struct WalletUnlocked {
     pub native_wallet_unlocked: RustOpaque<NativeWalletUnlocked>,
     pub private_key: String,
-    pub mnemonic_phrase: String,
 }
 
 impl WalletUnlocked {
+    // TODO: fix cannot borrow as mutable
+    // pub fn set_provider(&mut self, provider: Provider) {
+    //     let mut native_wallet = &mut*self.native_wallet_unlocked;
+    //     native_wallet.set_provider((&*provider.native_provider).clone())
+    // }
+
     pub fn new_random(provider: Option<Provider>) -> WalletUnlocked {
         let mut rng = rand::thread_rng();
         let mnemonic_phrase = generate_mnemonic_phrase(&mut rng, 12).unwrap();
         WalletUnlocked::from_mnemonic_phrase(mnemonic_phrase, provider)
     }
 
-    pub fn from_mnemonic_phrase(phrase: String, provider: Option<Provider>) -> WalletUnlocked {
-        let path = format!("{}/0'/0/0", DEFAULT_DERIVATION_PATH_PREFIX);
-        let secret_key = SecretKey::new_from_mnemonic_phrase_with_path(&phrase, &path).unwrap();
-        let native_provider = provider.map(|p| p.native_provider.try_unwrap().unwrap());
+    pub fn new_from_private_key(private_key: String, provider: Option<Provider>) -> WalletUnlocked {
+        let secret_key = SecretKey::from_str(private_key.as_str()).unwrap();
+        let native_provider = provider.map(|p| (&*p.native_provider).clone());
         let wallet_unlocked = NativeWalletUnlocked::new_from_private_key(secret_key, native_provider);
         Self {
             native_wallet_unlocked: RustOpaque::new(wallet_unlocked),
             private_key: secret_key.to_string(),
-            mnemonic_phrase: phrase,
+        }
+    }
+
+    pub fn from_mnemonic_phrase(phrase: String, provider: Option<Provider>) -> WalletUnlocked {
+        let path = format!("{}/0'/0/0", DEFAULT_DERIVATION_PATH_PREFIX);
+        WalletUnlocked::new_from_mnemonic_phrase_with_path(phrase, provider, path)
+    }
+
+    pub fn new_from_mnemonic_phrase_with_path(phrase: String, provider: Option<Provider>,
+                                              path: String) -> WalletUnlocked {
+        let secret_key = SecretKey::new_from_mnemonic_phrase_with_path(&phrase, &path).unwrap();
+        let native_provider = provider.map(|p| (&*p.native_provider).clone());
+        let wallet_unlocked = NativeWalletUnlocked::new_from_private_key(secret_key, native_provider);
+        Self {
+            native_wallet_unlocked: RustOpaque::new(wallet_unlocked),
+            private_key: secret_key.to_string(),
         }
     }
 
