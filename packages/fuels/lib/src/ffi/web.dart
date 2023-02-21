1088b2398607268c95c428a9097a0b6da6824fa3
mpoplavkov <mihail.poplavkov@gmail.com>
2023-02-21 18:25:21 +0100

    Flutter web platform support

--- packages/fuels/lib/src/ffi/web.dart
+++ packages/fuels/lib/src/ffi/web.dart
@@ -1,12 +1,22 @@
-import 'package:fuels/src/bridge_generated.dart';
+import 'dart:html';
+import 'package:fuels/src/bridge_generated.web.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
+
+const root = 'pkg/flutter_fuels';
 
 typedef ExternalLibrary = WasmModule;
 
 Fuels createWrapperImpl(ExternalLibrary module) => FuelsImpl.wasm(module);
 
-WasmModule createLibraryImpl() {
-  // TODO add web support. See:
-  // https://github.com/fzyzcjy/flutter_rust_bridge/blob/master/frb_example/with_flutter/lib/ffi.web.dart
-  throw UnsupportedError('Web support is not provided yet.');
+Future<WasmModule> createLibraryImpl() {
+  if (crossOriginIsolated != true) {
+    return Future.error(const MissingHeaderException());
+  }
+
+  final script = ScriptElement()..src = '$root.js';
+  document.head!.append(script);
+  return script.onLoad.first.then((_) {
+    eval("window.wasm_bindgen = wasm_bindgen");
+    return wasmModule.bind(wasmModule, '${root}_bg.wasm');
+  });
 }
