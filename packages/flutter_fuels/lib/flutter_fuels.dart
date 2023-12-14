10da263fd4ff9d823e238c4cf41323667411eea9
mpoplavkov <mihail.poplavkov@gmail.com>
2023-12-14 15:16:46 +0100

    feat: add exports for the model

--- packages/flutter_fuels/lib/flutter_fuels.dart
+++ packages/flutter_fuels/lib/flutter_fuels.dart
@@ -1,2 +1,14 @@
 export 'fuel_utils/fuel_utils.dart';
 export 'wallet/fuel_wallet.dart';
+
+// model
+export 'model/call_result.dart';
+export 'model/input.dart';
+export 'model/output.dart';
+export 'model/storage_slot.dart';
+export 'model/transaction.dart';
+export 'model/transaction_cost.dart';
+export 'model/transaction_receipt.dart';
+export 'model/tx_pointer.dart';
+export 'model/utxo_id.dart';
+export 'model/witness.dart';
